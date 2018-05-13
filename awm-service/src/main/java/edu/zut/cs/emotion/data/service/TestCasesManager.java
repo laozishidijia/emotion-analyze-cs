bf66f0d494ce05ae971b68760b8cfc9374326b61
wanghaing <1969323374@qq.com>
2018-05-13 14:52:01 +0800

    add services testcase and traincase

---
+++ awm-service/src/main/java/edu/zut/cs/emotion/data/service/TestCasesManager.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.emotion.data.service;
+
+import edu.zut.cd.emotion.base.service.GenericManager;
+import edu.zut.cs.emotion.admin.damain.TestCases;
+
+public interface TestCasesManager extends GenericManager<TestCases,Long>{
+
+}
---
+++ awm-service/src/main/java/edu/zut/cs/emotion/data/service/TrainCasesManager.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.emotion.data.service;
+
+import edu.zut.cd.emotion.base.service.GenericManager;
+import edu.zut.cs.emotion.admin.damain.TrainCases;
+
+public interface TrainCasesManager extends GenericManager<TrainCases,Long> {
+
+}
