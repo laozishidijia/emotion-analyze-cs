9d792313af1cb6673f41dbcd163e5f5c19bcff0e
wanghaing <1969323374@qq.com>
2018-05-13 14:54:38 +0800

    add test and train case_dao

---
+++ awm-dao/src/main/java/edu/zut/cs/emotion/admin/dao/TestCasesDao.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.emotion.admin.dao;
+
+import edu.zut.cs.emotion.admin.damain.TestCases;
+import edu.zut.cs.emotion.base.dao.GenericDao;
+
+public interface TestCasesDao extends GenericDao<TestCases,Long> {
+
+}
---
+++ awm-dao/src/main/java/edu/zut/cs/emotion/admin/dao/TrainCasesDao.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.emotion.admin.dao;
+
+import edu.zut.cs.emotion.admin.damain.TrainCases;
+import edu.zut.cs.emotion.base.dao.GenericDao;
+
+public interface TrainCasesDao extends GenericDao<TrainCases,Long>{
+
+}
