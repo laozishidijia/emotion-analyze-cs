76d1810076eee2421da2c7a42c35713f767148a8
beatifulxu <1975668635@qq.com>
2018-05-15 15:27:59 +0800

    test

---
+++ awm-service/src/test/java/edu/zut/cs/emotion/data/service/TestSampleManagerTest.java
@@ -0,0 +1,16 @@
+package edu.zut.cs.emotion.data.service;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.zut.cd.emotion.base.service.GenericGenerator;
+import edu.zut.cs.emotion.admin.damain.TestSample;
+
+public class TestSampleManagerTest extends GenericGenerator{
+	
+}
