26a2068847984aa63a46c65cce28c04c1713cb1a
duqingqing <dulovefighting.sina.com>
2018-05-15 16:55:35 +0800

    规范命名

--- awm-dao/src/main/java/edu/zut/cs/emotion/admin/dao/testdataDao.java
+++ awm-dao/src/main/java/edu/zut/cs/emotion/admin/dao/testdataDao.java
@@ -1,10 +1,10 @@
 package edu.zut.cs.emotion.admin.dao;
 
 
-import edu.zut.cs.emotion.admin.damain.testdata;
+import edu.zut.cs.emotion.admin.damain.TestData;
 import edu.zut.cs.emotion.base.dao.GenericDao;
 
-public interface testdataDao extends GenericDao<testdata, Long> {
+public interface testdataDao extends GenericDao<TestData, Long> {
 
 
 }
--- awm-dao/src/main/java/edu/zut/cs/emotion/admin/dao/traindataDao.java
+++ awm-dao/src/main/java/edu/zut/cs/emotion/admin/dao/traindataDao.java
@@ -1,8 +1,8 @@
 package edu.zut.cs.emotion.admin.dao;
 
-import edu.zut.cs.emotion.admin.damain.traindata;
+import edu.zut.cs.emotion.admin.damain.TrainData;
 import edu.zut.cs.emotion.base.dao.GenericDao;
 
-public interface traindataDao extends GenericDao<traindata, Long> {
+public interface traindataDao extends GenericDao<TrainData, Long> {
 
 }
