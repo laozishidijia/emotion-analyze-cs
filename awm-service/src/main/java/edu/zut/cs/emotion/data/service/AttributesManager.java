cd380fc0b1f7ecf4a877d7dab8711a8e86d5f266
wanghaing <1969323374@qq.com>
2018-05-27 21:37:21 +0800

    添加AttributesManager

---
+++ awm-service/src/main/java/edu/zut/cs/emotion/data/service/AttributesManager.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.emotion.data.service;
+
+import edu.zut.cs.emotion.admin.domain.Attributes;
+import edu.zut.cs.emotion.base.service.GenericManager;
+
+public interface AttributesManager extends GenericManager<Attributes, Long>{
+
+}
