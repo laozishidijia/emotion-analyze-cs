97f038a65bbcd4c3846820fb08330698fdaf3a84
wanghaing <1969323374@qq.com>
2018-05-27 21:42:52 +0800

    加入AttributesDao

---
+++ awm-dao/src/main/java/edu/zut/cs/emotion/admin/dao/AttributesDao.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.emotion.admin.dao;
+
+import edu.zut.cs.emotion.admin.domain.Attributes;
+import edu.zut.cs.emotion.base.dao.GenericDao;
+
+public interface AttributesDao extends GenericDao<Attributes, Long>{
+
+}
