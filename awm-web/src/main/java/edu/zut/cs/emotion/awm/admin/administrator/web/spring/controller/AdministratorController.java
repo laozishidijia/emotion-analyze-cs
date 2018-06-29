1fd9c5f22830a7e71380cd89273ea79f8c8dcd28
duqingqing <dulovefighting.sina.com>
2018-06-29 15:16:55 +0800

    修改andmin的controller实习登入

--- awm-web/src/main/java/edu/zut/cs/emotion/awm/admin/administrator/web/spring/controller/AdministratorController.java
+++ awm-web/src/main/java/edu/zut/cs/emotion/awm/admin/administrator/web/spring/controller/AdministratorController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import edu.zut.cs.emotion.admin.administrator.domain.Administrator;
 import edu.zut.cs.emotion.administrator.service.AdministratorManager;
@@ -31,37 +32,28 @@ public class AdministratorController extends GenericController<Administrator,Lon
 			{
 				return "/administrator/login";
 			}else {
-				String sessionusername = (String) request.getSession().getAttribute("username");
-				String sessionpassword = (String) request.getSession().getAttribute("password");
-				Administrator admin= administratorManager.findByUsername(sessionusername);
-				if(admin.getPassword().equals(sessionpassword)) {
-					
-				}else {
-					return "/administrator/login";
-				}
+				return "/administrator/index";
 			}
-			return "/administrator/index.html";
 	}
 	@RequestMapping(value ="/login.html",method = RequestMethod.POST)
-	public String login(HttpServletRequest request)
+	public ModelAndView login(HttpServletRequest request)
 	{
+			ModelAndView mov = new ModelAndView();
 			HttpSession session = request.getSession();
 			String result="/administrator/login";
 		    String key="98k";
 		 	String username=request.getParameter("username");
 	        String password=request.getParameter("password");
-	        System.out.println("【用户名】"+username);
-	        System.out.println("【密码】"+password);
-	        
 	        String encodedpassword=DigestUtils.md5Hex(password + key);
 	        Administrator admin= administratorManager.findByUsername(username);
 	        if(admin.getPassword().equals(encodedpassword))
 	        {
-	        	System.out.println("================登入成功================");
 	        	session.setAttribute("username", username);
 	        	session.setAttribute("password", encodedpassword);
-	        	return "/administrator/home.html";
+	        	mov.addObject("username", username);
+	        	result= "/administrator/index";
 	        }
-	    	return result;
+	        mov.setViewName(result);
+	    	return mov;
 	}
 }
